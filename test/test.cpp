d1192d7cb07c3dc5fbcd255c229dbaf26aec8280
Riccardo <riccardo.cerberi@gmail.com>
2023-02-08 12:39:40 +0100

    add constant.hpp header

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,21 @@
 #include <cstdint>
 #include<iostream>
 #include<cstdio>
+#include<array>
 
-#define BASE 0x80000000
-#define SIZE 3
+class Log {
+public:
+    enum Level {
+        error = 0, warning, info
+    };
+    void setLevel(Level);
+    void error(char c);
+};
+
+
 
 int main() {
-    uint64_t arr[SIZE]={1,5,7};
-    uint64_t base_ptr = (uint64_t) &arr[0];
-    uint64_t p0 = BASE, p1 = BASE + 1;
+    Log l;
 
-
-    // without sizeof() it will cause an error because it didn't switch to the next
-    // element causing a wrong read
-    std::cout << *((uint64_t*) (base_ptr + (p1-p0)*sizeof(uint64_t))) << "\n";
-
+    l.setLevel(Level::error);
 }
