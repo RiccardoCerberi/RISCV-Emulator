995eb0122f6d963a04851ec8bd45c909962ce6a1
Riccardo <riccardo.cerberi@gmail.com>
2023-02-08 18:15:20 +0100

    change file path length passed as command line argument

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 
 #include"emulator/emulator.hpp"
 
+#define MAX_FILE_LENGTH 50
 
 int main(int argc, char** argv) {
     int index_file = 1;
 
 
-    if (argc < 2 || (std::strlen(argv[index_file]) > 20)) {
+    if (argc < 2 || (std::strlen(argv[index_file]) > MAX_FILE_LENGTH)) {
         std::cerr << "Error: \n";
         return -1;
     }
