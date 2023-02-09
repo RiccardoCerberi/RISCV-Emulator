93f2f40c3c106d167767c937bcaf5d17a5ee27b6
Riccardo <riccardo.cerberi@gmail.com>
2023-02-09 18:41:58 +0100

    the path of the binary file is relative to the executable

--- src/emulator/emulator.cpp
+++ src/emulator/emulator.cpp
@@ -7,8 +7,9 @@
 
 Emulator::Emulator(std::string const &file_name)
 {
-    std::ifstream input_file(file_name,  std::ios::binary);
+    std::ifstream input_file;
 
+    input_file.open(file_name,  std::ios::binary);
     assert(input_file.is_open() == true);
 
     uint64_t address_tostore_instruction = kdram_base;
@@ -20,4 +21,5 @@ Emulator::Emulator(std::string const &file_name)
         address_tostore_instruction += data_size::kword;
     }
     m_dram.setLastInstructionAddress(address_tostore_instruction - static_cast<uint64_t>(data_size::kword));
+    std::cout << "Instructions are constructed\n";
 }
