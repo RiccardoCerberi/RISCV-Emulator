2703257b6179cbfe5dd458a57f3127d27956b621
riccardo <riccardo.cerberi@gmail.com>
2023-07-17 21:00:18 +0200

    semplified csrs.cpp file: no subclasses, everything is distinguished by the execute function

--- include/instructions/csrs.hpp
+++ include/instructions/csrs.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
-#include"system.hpp"
-#include"../bits-manipulation.hpp"
 #include <cstdint>
 
+#include "../bits-manipulation.hpp"
+#include "system.hpp"
+
 /*
- * Every instruction of this type consists in 
+ * Every instruction of this type consists in
  * - Reading value from csrs
  * - doing some operations, producing the result
  * - swapping the result with register rd
@@ -16,78 +17,18 @@ class CSRAux;
 class CSR : public System {
 public:
     CSR(uint32_t const is, uint64_t const pc);
-    void readRegister(reg_type const&) override;
+    void readRegister(reg_type const &) override;
     void readCSR(const csrs_t &) override;
-// execution function makes the result for modify the csr
-// register at index m_index_csr
     void execution() override;
-    void writeCsr(csrs_t& csrs) override;
+    void writeCsr(csrs_t &csrs) override;
     void writeBack(reg_type &) override;
+
 private:
-    CSRAux* m_csr_aux;
-    uint16_t const& m_index_csr;
-    uint64_t m_rs1;
-    uint64_t m_csr_rs; // alias t in riscv instructions
+    uint64_t makeCSRResult();
+
+private:
+    uint64_t m_second_operand;
+    uint64_t m_csr_rs;  // alias t in riscv instructions
     uint64_t m_csr_rd;
     uint64_t m_rd;
-}; 
-
-
-/*
- * CSRAUx defines the interface for immediate and non operations with CSRs
- */
-
-class CSRAux {
-protected:
-    using func3_t = System::func3_t;
-public:
-    CSRAux(System::func3_t const func3, uint8_t const qty) 
-        : m_func3(func3), m_qty(qty)
-    {}
-// If the CSRs instructions is a non immediate operations, then it will read
-// data from the normal registers (the ones from 0 to 31) based on the qty, in fact it becomes an index in the registers vector;
-// otherwise the quantity refers to an immediate, so it will use it just for making
-// the result 
-    virtual void InterpretQty(reg_type const&) {}
-    virtual uint64_t makeCSRResult(uint64_t const) = 0;
-protected:
-    func3_t m_func3;
-    uint8_t m_qty; //it can be either an immediate for csri operations or an id to distinguish non-immediate operations
 };
-
-class CSRNotImm : public CSRAux {
-public: 
-    CSRNotImm(func3_t const func3, uint8_t const qty) 
-        : CSRAux(func3, qty)
-    {}
-    // in this case, qty represents the index of the register
-    // rs1, which is the operand in the functions srrw etc
-    void InterpretQty(reg_type const&) override;
-    // based on the qty it executes one function between
-    // srrw, srrr, srrc
-    uint64_t makeCSRResult(uint64_t const) override;
-private:
-    uint64_t csrrw(uint64_t const);
-    uint64_t csrrs(uint64_t const);
-    uint64_t csrrc(uint64_t const);
-
-// it stores the content of the register at qty index.
-    uint64_t m_rs1; 
-};
-
-class CSRImm : public CSRAux {
-public: 
-    CSRImm(func3_t const func3, uint8_t const qty) 
-        : CSRAux(func3,qty)
-    {}
-    // it zero-extends the immediate 
-    void InterpretQty(reg_type const&) override; 
-    uint64_t makeCSRResult(uint64_t const) override;
-private:
-    uint64_t csrrwi(uint64_t const);
-    uint64_t csrrsi(uint64_t const);
-    uint64_t csrrci(uint64_t const);
-
-    uint64_t m_imm;
-};
-
--- src/instructions/csrs.cpp
+++ src/instructions/csrs.cpp
@@ -1,100 +1,41 @@
-#include"../../include/instructions/csrs.hpp"
+#include "../../include/instructions/csrs.hpp"
+
 #include <cstdint>
 
-CSR::CSR(uint32_t const is, uint64_t const pc) 
-        : System(is,pc), m_index_csr(m_func12)
-    {
-        // distinguish between immediate and non operations
-        if (static_cast<uint8_t>(m_func3) < 5) { 
-            m_csr_aux = new CSRImm(m_func3, BitsManipulation::takeBits(is,15,19));
-        } else {
-            m_csr_aux = new CSRNotImm(m_func3, BitsManipulation::takeBits(is,15,19));
-        }
-    }
+CSR::CSR(uint32_t const is, uint64_t const pc) : System(is, pc) {}
 
-void CSR::readRegister(reg_type const& reg) { 
-    m_csr_aux->InterpretQty(reg);
+void CSR::readRegister(reg_type const& reg) {
+    m_second_operand = BitsManipulation::takeBits(m_instruction, 15, 19);
+    // for non immediate function the bits form the register index
+    if (static_cast<uint8_t>(m_func3) >= 5)
+        m_second_operand = reg[m_second_operand];
 }
-    
-void CSR::readCSR( csrs_t const& csrs) {
-    m_csr_rs = csrs[m_index_csr];
-}
+
+void CSR::readCSR(csrs_t const& csrs) { m_csr_rs = csrs[m_func12]; }
 
 void CSR::execution() {
-    m_csr_rd = m_csr_aux->makeCSRResult(m_csr_rs);
-    m_rd = m_csr_rs; 
+    m_csr_rd = makeCSRResult();
+    m_rd = m_csr_rs;
 }
 
-void CSR::writeCsr(csrs_t& csrs) {
-    csrs[m_index_csr] = m_csr_rd;
-}
+void CSR::writeCsr(csrs_t& csrs) { csrs[m_func12] = m_csr_rd; }
 
-void CSR::writeBack(reg_type &reg) {
+void CSR::writeBack(reg_type& reg) {
     if (m_index_rd != 0) reg[m_index_rd] = m_rd;
 }
 
-void CSRNotImm::InterpretQty( reg_type const & reg) {
-    m_rs1 = reg[m_qty];
-}
-
-uint64_t CSRNotImm::makeCSRResult(uint64_t const csr_rs) {
-    uint64_t result = 0;
-    switch(m_func3) {
-        case func3_t::kcsrrw:
-            result = csrrw(csr_rs);
-            break;
-        case func3_t::kcsrrs:
-            result = csrrs(csr_rs);
-            break;
-        case func3_t::ksrrc:
-            result = csrrc(csr_rs);
-            break;
-        default:
-            std::cerr << "Error: csrs not immediate cases not covered\n";
-            abort();
-    }
-    return result;
-}
-
-uint64_t CSRNotImm::csrrw(uint64_t const) {
-    return m_rs1;  
-}
-
-uint64_t CSRNotImm::csrrs(uint64_t const csr_rs) {
-    return arith(csr_rs, m_rs1, "|");
-}
-
-uint64_t CSRNotImm::csrrc(uint64_t const csr_rs) {
-    return arith(csr_rs, !m_rs1, "&");
-}
-
-void CSRImm::InterpretQty(reg_type const&) {
-    m_imm = static_cast<uint64_t>(m_imm);
-}
-
-uint64_t CSRImm::makeCSRResult(uint64_t const csr_rs) {
-    switch(m_func3) {
-        case func3_t::kcsrrwi:
-            return csrrwi(csr_rs);
-        case func3_t::ksrrsi:
-            return csrrsi(csr_rs);
-        case func3_t::kcsrrci:
-            return csrrci(csr_rs);
-        default:
-            std::cerr << "Error in CSRImm member function: m_func3 didn't match previous case\n";
-            abort();
+uint64_t CSR::makeCSRResult() {
+    if (m_func3 == System::func3_t::kcsrrw ||
+        m_func3 == System::func3_t::kcsrrwi) {
+        return m_second_operand;
+    } else if (m_func3 == System::func3_t::kcsrrs ||
+               m_func3 == System::func3_t::ksrrsi) {
+        return arith(m_csr_rs, m_second_operand, "|");
+    } else if (m_func3 == System::func3_t::ksrrc ||
+               m_func3 == System::func3_t::kcsrrci) {
+        return arith(m_csr_rs, !m_second_operand, "&");
+    } else {
+        std::cerr << "Error: func3 is invalid\n";
+        abort();
     }
 }
-
-uint64_t CSRImm::csrrwi(uint64_t const) {
-    return m_imm;
-}
-
-uint64_t CSRImm::csrrsi(uint64_t const csr_rs) {
-    return arith(csr_rs, m_imm, "|");
-}
-
-uint64_t CSRImm::csrrci(uint64_t const csr_rs) {
-    return arith(csr_rs, !m_imm, "&");
-}
-
