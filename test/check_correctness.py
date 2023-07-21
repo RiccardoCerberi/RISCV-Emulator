c9bff16f7534edb6820b158af0189e59dc3463b5
riccardo <riccardo.cerberi@gmail.com>
2023-07-21 17:48:17 +0200

    arithmetic shift: sign extend not only vacant bit

--- test/check_correctness.py
+++ test/check_correctness.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
-
 import subprocess
 import glob
 
 # Enable test for emulator.
 # For each binaries it checks the presence of  jump instruction to <pass> and the absence of any jump to <fail>
-TARGET = '<fail>:'
-
 EXEC_PATH = '../bin/Debug/riscv-emulator'
 
 OUT_FILE = 'out.txt'
 
 def executeTest(bin_file):
-    subprocess.run(f'{EXEC_PATH} test/bin_files/{bin_file} > {OUT_FILE}',
+    subprocess.run(f'{EXEC_PATH} bin_files/{bin_file} > {OUT_FILE}',
                    shell=True)
+
+def findTargetInstruction(s, target):
+    beg_target = s.rfind(target)
+    beg_ins = s[:beg_target].rfind('\n')+1
+    return  s[beg_ins : beg_target-1]
 
 if __name__ == '__main__':
     test_num = 1
     with open(OUT_FILE) as out:
         for dump_f in glob.glob('dump_files/*.dump'):
             print(f'Testing file: {dump_f}')
-            executeTest(dump_f.replace('.dump', '.bin'))
+            #remove dump_files and .dump from file passed to executeTest
+            #function
+            executeTest(dump_f[dump_f.find('/')+1:].replace('.dump', '.bin'))
             with open(dump_f) as df:
                 s = df.read()
-                beg_target = s.rfind(TARGET)
-                beg_line = s[:beg_target].rfind('\n')+1
-                fail_ins = s[beg_line: beg_target-1]
+                ins_fail = findTargetInstruction(s, '<fail>:')
+                print(f'fail instruction: {ins_fail}')
+                ins_succ = findTargetInstruction(s, '<pass>:')
+                print(f'fail instruction: {ins_succ}')
+                if (out.read().find(ins_fail) == -1 
+                    and out.read().find(ins_succ) != -1):
+                    print('SUCESS')
+                else:
+                    print('FAILURE')
+                    break
                 if (out.read().find(fail_ins) != -1):
                     print(f"FAILED on test {test_num}")
                     break
