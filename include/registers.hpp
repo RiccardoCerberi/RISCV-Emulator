d5dfb3adb21f9c9158d7c06533d6d678bcd50820
riccardo <riccardo.cerberi@gmail.com>
2023-08-05 18:23:36 +0200

    change abi_names to static constexpr to make array statically

--- include/registers.hpp
+++ include/registers.hpp
@@ -6,8 +6,11 @@
 
 class Registers {
 public:
-    static std::array<std::string, ktot_registers> abi_names;
-    static std::string                             printABIName(size_t indx);
+    static constexpr std::array<std::string, ktot_registers> abi_names{
+        "zero", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
+        "a1",   "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
+        "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+    static std::string printABIName(size_t indx);
 
 public:
     Registers() : m_registers{0} {
--- src/registers.cpp
+++ src/registers.cpp
@@ -1,10 +1,5 @@
 #include "../include/registers.hpp"
 
-std::array<std::string, ktot_registers> Registers::abi_names{
-    "zero", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
-    "a1",   "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
-    "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
-
 std::string Registers::printABIName(size_t indx) {
     assert(indx < ktot_registers);
     return abi_names[indx];
